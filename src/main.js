6c656279f35457f1148f48a5f530be51d749b183
elliot-mb <elliotbuckingham14@gmail.com>
2022-11-01 23:12:01 +0000

    initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("canvas"); 
+var c = canvas.getContext("2d"); 
+const INITIAL_WIDTH = 1920;
+const INITIAL_HEIGHT = 1080;
+
+c.fillStyle = "#0f0";
+c.fillRect(0, 0, INITIAL_WIDTH, INITIAL_HEIGHT);
