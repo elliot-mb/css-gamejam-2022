42696ba03152b25e9f8bf99a0155c5d8c8fad21b
Edward Su <edsusiyuan@gmail.com>
2022-11-02 14:21:45 +0000

    constructor for entity.js

--- src/entity.js
+++ src/entity.js
@@ -1,6 +1,5 @@
 /* TODO 
 props:
-    ray 
     position 
     health //tiles dubiously need health???
     hitbox
@@ -13,6 +12,61 @@ methods:
     draw() 
 */
 
-export default class Entity {
+const spriteDimensions = 16;
+const spriteAnimations = [];
 
+export default class Entity {
+    constructor(properties){
+        this.nametag = properties.nametag;
+        this.pos = properties.pos;
+        this.visible = properties.visible;
+        this.colliding = []; //comprehensive list of collisions with current object
+        this.collides = properties.collides;
+        this.ctx = properties.ctx;
+        this.spriteInfo = properties.spriteInfo;
+        //this.dims = properties.dims;
+        //this.vel = properties.vel;        
+        //this.camera = properties.camera;
+        //this.colour = properties.colour;
+        //this.jumpable = properties.jumpable;
+        //this.grav = properties.grav; //is effected by gravity
+        //this.simulated = properties.simulated;
+        //this.drag = properties.drag;
+
+        this.spriteInfo.array.forEach((state, index) => {
+            let frames = {
+                loc: [],
+            }
+
+            for (let i = 0; i < state.frames; i++) {
+                let positionX = i * spriteDimensions;
+                let positionY = index * spriteDimensions;
+                frames.loc.push({x: positionX, y: positionY});
+            }
+            spriteAnimations[state.name] = frames
+        });
+
+
+    }
+
+    move(target) {
+        
+    }
+
+    update() {
+
+    }
+
+    animate() {
+
+    }
+
+    draw() {
+
+    }
+
+    collide() {
+
+    }
+    
 }
