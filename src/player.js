ece149760b6ff9c2aba77ce04981737c63ab40d5
Edward Su <edsusiyuan@gmail.com>
2022-11-02 23:33:34 +0000

    added playerimage finding

--- src/player.js
+++ src/player.js
@@ -126,5 +126,6 @@ export default class Player extends Entity {
         c.fillStyle = this.coolingDown(ts) ? "#920" : "#fff";
         //console.log(this.coolingDown(ts));
         c.fillRect(this.visualPos[0], this.visualPos[1], this.scale, this.scale);
+        
     }
 }
