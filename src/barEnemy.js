c5017752e2e351e11f22deca602bb286abdb7499
Leo_Lai <leojacklai@outlook.com>
2022-11-03 02:49:02 +0000

    adding bar enemy class

---
+++ src/barEnemy.js
@@ -0,0 +1,11 @@
+import Enemy from "./enemy.js"
+
+export default class barEnemy extends Enemy{
+
+    constructor(_properties) {
+        super(_properties);
+        this.timeStamp = 0;
+    }
+
+    //void function which updates properties and changes colours
+}
